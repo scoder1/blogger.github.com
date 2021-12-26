9135eb0a8c6d1bad411c80b7a0f5de2d4640c6ec
CodingWithSatyam <tubex998@gmail.com>
2021-12-26 08:08:32 +0545

    Scoder
    This website will going to help you to help you to learn coding

---
+++ script.js
@@ -0,0 +1,16 @@
+// Munu Icon desplay
+var menubtn = document.getElementById("menubtn");
+var sidenav = document.getElementById("sidenav");
+var menu = document.getElementById("menu");
+
+sidenav.style.right = "-250px";
+
+menubtn.onclick = function () {
+  if (sidenav.style.right == "-250px") {
+    sidenav.style.right = "0";
+    menu.src = "photos/close.png";
+  } else {
+    sidenav.style.right = "-250px";
+    menu.src = "photos/menu.png";
+  }
+};
